7b9017aac57ce0143327f0a79a93e5e873317250
andrew <october072017@yandex.ru>
2021-07-11 15:19:09 +0600

    UserServiceTest.

--- src/test/java/com/github/andrewdnv/service/user/MockObjects.java
+++ src/test/java/com/github/andrewdnv/service/user/MockObjects.java
@@ -1,8 +1,10 @@
 package com.github.andrewdnv.service.user;
 
+import com.github.andrewdnv.service.user.entity.UserDo;
 import com.github.andrewdnv.service.user.model.User;
 import com.github.andrewdnv.service.user.model.UserStatus;
 
+import java.sql.Date;
 import java.time.LocalDate;
 
 public class MockObjects {
@@ -19,6 +21,20 @@ public class MockObjects {
             .setMobilePhone("+71001001010")
             .setEmail("user@mail.com")
             .setStatus(UserStatus.ACTIVE);
+    }
+
+    public static UserDo userDo() {
+        return new UserDo()
+            .setId(1L)
+            .setFirstName("firstName")
+            .setLastName("lastName")
+            .setPatronymicName("patronymic")
+            .setDateOfBirth(Date.valueOf(LocalDate.of(1990, 1, 24)))
+            .setDateOfRegistration(Date.valueOf(LocalDate.of(2020, 1, 24)))
+            .setCity("city")
+            .setMobilePhone("+71001001010")
+            .setEmail("user@mail.com")
+            .setStatus(UserStatus.ACTIVE.getValue());
     }
 
 }
--- src/test/java/com/github/andrewdnv/service/user/controller/UserControllerTest.java
+++ src/test/java/com/github/andrewdnv/service/user/controller/UserControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest
 @AutoConfigureMockMvc
+@ActiveProfiles("unitTest")
 public class UserControllerTest {
 
     @MockBean
