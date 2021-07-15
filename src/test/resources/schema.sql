e58dbe1d454123a4667eaa02eb2257918d09d6ea
andrew <october072017@yandex.ru>
2021-07-15 23:21:37 +0600

    UserDaoTest.

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -4,10 +4,10 @@ CREATE TABLE users
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     patronymic_name VARCHAR(255),
-    date_of_birth TIMESTAMP,
-    date_of_registration TIMESTAMP NOT NULL,
+    date_of_birth DATE,
+    date_of_registration DATE NOT NULL,
     city VARCHAR(255),
     mobile_phone VARCHAR(15),
     email VARCHAR(127),
     status VARCHAR(31) NOT NULL
-)
+);
